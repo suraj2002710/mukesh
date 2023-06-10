0f55d9b9924b6f0110b1f7da1040eca64e17a738
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-06-10 10:46:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors(
 app.set('views','views')
 app.set('view engine', 'ejs')
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://socket-client.onrender.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
