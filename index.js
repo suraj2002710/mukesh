227bef9f92d9061d2793f91ee446d2e9f437286f
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-06-10 11:19:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(cors(
 ))
 app.set('views','views')
 app.set('view engine', 'ejs')
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001/');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.use(fileUpload())
 // app.get('/', (req, res) => {
